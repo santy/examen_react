9c32658b68f67bfbac7cfd98bbd3417d1016d609
Santy Cierco <santidisturbios@gmail.com>
2023-11-03 10:07:41 +0100

    update

---
+++ src/FoodApp.js
@@ -0,0 +1,26 @@
+//Modificar para usar useContext
+//FoodApp, Menu, Order
+
+import React, { useState } from "react";
+import Menu from "./Menu";
+const FoodApp = (props) => {
+  const [shopOpen, setShopOpen] = useState("Open");
+  const [btnText, setBtnText] = useState("Close");
+  const openOrCloseShop = () => {
+    if (shopOpen === "Open") {
+      setShopOpen("Closed");
+      setBtnText("Open");
+    } else {
+      setShopOpen("Open");
+      setBtnText("Closed");
+    }
+  };
+  return (
+    <>
+      <h1>Food Shop is now {shopOpen}</h1>
+      <button onClick={openOrCloseShop}>{btnText}</button>
+      <Menu isOpen={shopOpen} />
+    </>
+  );
+};
+export default FoodApp;
